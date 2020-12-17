a9084a5ff871871314d2175888ba22e277663c91
NSuwal039 <57157948+NSuwal039@users.noreply.github.com>
2020-12-17 12:16:09 +0545

    Add files via upload

---
+++ Comparator.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+class Player {
+    String name;
+    int score;
+
+    Player(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+}
+
+class Checker implements Comparator<Player> {
+    // complete this method
+    public int compare(Player a, Player b) {
+        int result = 0;
+        if (a.score>b.score) result=1;
+        else if (a.score<b.score) result=-1;
+        else {
+            int compare = a.name.compareTo(b.name);
+            if (compare<0) result=-1;
+            else result=1;
+        }
+
+        return result;
+    }
+}
+
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        Player[] player = new Player[n];
+        Checker checker = new Checker();
+
+        for(int i = 0; i < n; i++){
+            player[i] = new Player(scan.next(), scan.nextInt());
+        }
+        scan.close();
+
+        Arrays.sort(player, checker);
+        for(int i = 0; i < player.length; i++){
+            System.out.printf("%s %s\n", player[i].name, player[i].score);
+        }
+    }
+}
